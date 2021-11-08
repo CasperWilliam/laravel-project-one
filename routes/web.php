3b94b25d1d033d214e7dfe2b831001ac0ae1b8a2
Lucas Siefert <theapemachine@Lucass-Air.attlocal.net>
2021-11-08 12:46:59 -0500

    More routing works

--- routes/web.php
+++ routes/web.php
@@ -13,20 +13,82 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return 'Home Page';
-})->name('home.index');
+// Route::get('/', function () {
+//     return view('home.index', []);
+// })->name('home.index');
 
-Route::get('/contact', function() {
-    return 'Contact';
-})->name('home.contact');
+// Route::get('/contact', function () {
+//     return view('home.contact');
+// })->name('home.contact');
 
-Route::get('/posts/{id}', function ($id) {
-    return 'Blog post ' . $id;
+Route::view('/', 'home.index')
+->name('home.index');
+Route::view('/contact', 'home.contact')
+->name('home.contact');
+
+$posts = [
+    1 => [
+        'title' => 'Intro to Laravel',
+        'content' => 'This is a short intro to Laravel',
+        'is_new' => true,
+        'has_comments' => true
+    ],
+    2 => [
+        'title' => 'Intro to PHP',
+        'content' => 'This is a short intro to PHP',
+        'is_new' => false
+    ],
+    3 => [
+        'title' => 'Intro to Laravel',
+        'content' => 'This is a short intro to Golang',
+        'is_new' => false,
+    ]
+];
+
+Route::get('/posts', function() use($posts) {
+    return view('posts.index', ['posts' => $posts]);
+});
+
+
+Route::get('/posts/{id}', function ($id) use($posts) {
+
+    abort_if(!isset($posts[$id]), 404);
+
+    return view('posts.show', ['post' => $posts[$id]]);
 })->where([
     'id' => '[0-9]+'
 ])->name('posts.show');
 
-Route::get('recent-posts/{days_ago?}', function ($daysAgo = 20){
-    return 'Posts from ' .$daysAgo . ' days ago';
+Route::get('recent-posts/{days_ago?}', function ($daysAgo = 20) {
+    return 'Posts from ' . $daysAgo . ' days ago';
 })->name('posts.recent.index');
+
+Route::get('fun/responses', function () use($posts){
+return response($posts, 201)
+->header('Content-Type', 'application/json')
+->cookie('MY_COOKIE', 'Luke Siefert', 3600);
+});
+
+Route::get('/fun/redirect', function(){
+    return redirect('/contact');
+});
+
+Route::get('/fun/back', function(){
+    return back();
+});
+
+Route::get('/fun/named-route', function(){
+    return redirect()->route('posts.show', ['id' => 1]);
+});
+
+Route::get('/fun/away', function(){
+    return redirect()->away('https://google.com');
+});
+
+Route::get('/fun/json', function () use($posts) {
+ return response()->json($posts);
+});
+
+Route::get('/fun/download', function(){
+    return response()->download(public_path('/daniel.jpg'), 'daniel');
+});
